457150a985d6de38de720bf3158a69a809e2c845
Emre AKA <aka548548@gmail.com>
2024-04-15 23:58:08 +0300

    BFS and DFS algorithms on Directed, No cycle graphs.

---
+++ DepthFirstAndBreadthFirstAlgorithms/Program.cs
@@ -0,0 +1,23 @@
+﻿using DepthFirstAndBreadthFirstAlgorithms;
+
+Console.WriteLine("Hello, World!");
+
+var graph = new Dictionary<string, string[]>()
+{
+    { "a", ["c", "b"] },
+    { "b", ["d"] },
+    { "c", ["e"] },
+    { "d", ["f"] },
+    { "e", [] },
+    { "f", [] },
+};
+
+
+Console.WriteLine("Depth First Search:");
+Algorithms.DepthFirstPrint(graph, "a");
+
+Console.WriteLine("Depth First Recursive Search:");
+Algorithms.DepthFirstRecursivePrint(graph, "a");
+
+Console.WriteLine("Breadth First Search:");
+Algorithms.BreadthFirstPrint(graph, "a");
